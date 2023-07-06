abfd04d1f13d8501054f0f4e72a3904b8c5b4198
Sambhav <jainsmbhv22@gmail.com>
2023-07-06 22:02:23 +0530

    hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { UserDataService } from './services/user-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'sample';
+  users: any;
+  constructor(private userData: UserDataService) {
+    userData.users().subscribe((Data) => {
+      console.warn(Data);
+    });
+  }
 }
---
+++ src/app/services/user-data.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserDataService {
+  constructor(private http: HttpClient) {}
+  users() {
+    return this.http.get('https://jsonplaceholder.typicode.com/users');
+  }
+}
